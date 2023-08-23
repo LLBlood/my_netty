63e63d6d00f911d969d92c14eb362454db3b1575
liulin <liulin@51carfi.com>
2023-08-23 16:57:44 +0800

    task #b, JbossMarshalling

---
+++ src/main/java/cn/liulin/my_netty/Netty/jbossmarshalling/SubReqClient.java
@@ -0,0 +1,44 @@
+package cn.liulin.my_netty.Netty.jbossmarshalling;
+
+import cn.liulin.my_netty.Netty.protobuf.SubReqClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * cn.liulin.my_netty.Netty.jbossmarshalling$
+ *
+ * @author ll
+ * @date 2023-08-23 16:42:30
+ **/
+public class SubReqClient {
+    public void connect(int port, String host) throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingDecoder());
+                            socketChannel.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingEncoder());
+                            socketChannel.pipeline().addLast(new SubReqClientHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SubReqClient().connect(8080, "127.0.0.1");
+    }
+}
